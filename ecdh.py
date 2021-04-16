aadbbf87e81c8e6a37e491f35a9e996fccb50987
JohnSmith72 <chris.sc.lam29@gmail.com>
2021-04-16 19:15:29 +1000

    Cleaned up ecdh.py

--- ecdh.py
+++ ecdh.py
@@ -1,13 +1,11 @@
 # Testing generation of 128-bit EphID and generation of EncID
 
 # Issue:
-# 	Generating EncID requires 17byte input
-# 	EphID is required to be 16 bytes
-# 	Need to reconstruct a 17byte ephID at receiver end
+# 	load_received_public_key_bytes from ecdsa requires 17byte public key as input
 # 		Solution:
 # 			1. Use hash to determine if EphID was prepended with \x02 or \x03
 # 			2. Use only one type (e.g. only EphID starting with \x02)
-# 			3. Doesnt seem to actually make a difference (I dont know the maths behind ECDH) (Currently used)
+# 			3. Append 'b\x03' to EphID. Sign of Y does not seem to make a difference to EncID generated (Currently used)
 
 # Setup:
 # 	git clone https://github.com/tlsfuzzer/python-ecdsa.git
@@ -15,53 +13,45 @@
 # 	ensure ecdsa not pip installed
 # 	mv src ecdsa
 
+from hashlib import md5
 from ecdsa import ECDH, SECP128r1
-import hashlib
 
 def generateECDHObjects ():
 	ecdh = ECDH(curve=SECP128r1)
 	ecdh.generate_private_key()
-	public = ecdh.get_public_key().to_string("compressed")
+	publicKey = ecdh.get_public_key().to_string("compressed")[1:]
 
 	return (
 		ecdh,
-		public[1:],
-		hashlib.md5(public).hexdigest()
+		publicKey,
+		md5(publicKey).hexdigest()[:6]
 	)
 
-# load_received_public_key_bytes() requires 17 byte input
-# Using solution 3
-def reconstructPublic (ephID, hash):
-	return b'\x02' + ephID
+# Verify reconstructed EphID matches first 6 bytes of EphID hash from packet
+def verifyEphID (EphID, receivedHash):
+	return md5(EphID).hexdigest()[:6] == receivedHash
 
-# Using solution 1
-def reconstructPublic2 (ephID, hash):
-	posEphID = b'\x03' + ephID
-	negEphID = b'\x02' + ephID
-	hashPosEphID = hashlib.md5(posEphID).hexdigest()
-	if (hashPosEphID == hash):
-		return posEphID
-	else:
-		return negEphID
+# Append with b'\x02' since required and it does not affect EncID generation
+def calcEncID (ECDH, receivedEphID):
+	ECDH.load_received_public_key_bytes(b'\x02' + receivedEphID)
+	return ECDH.generate_sharedsecret_bytes().hex()
 
 def EphIDTest ():
 	alice, aliceEphID, aliceHash = generateECDHObjects()
 	bob, bobEphID, bobHash = generateECDHObjects()
-	aliceReconstructed = reconstructPublic(aliceEphID, aliceHash)
-	bobReconstructed = reconstructPublic(bobEphID, bobHash)
 
-	alice.load_received_public_key_bytes(bobReconstructed)
-	bob.load_received_public_key_bytes(aliceReconstructed)
+	assert (verifyEphID(aliceEphID, aliceHash))
+	assert (verifyEphID(bobEphID, bobHash))
 
-	aliceEncID = alice.generate_sharedsecret_bytes()
-	print(aliceEncID.hex())
+	aliceEncID = calcEncID(alice, bobEphID)
+	bobEncID = calcEncID(bob, aliceEphID)
 
-	bobEncID = bob.generate_sharedsecret_bytes()
-	print(bobEncID.hex())
+	print(aliceEncID)
+	print(bobEncID)
 
 	if (aliceEncID == bobEncID):
-		print("Success!")
+		print(f"Success: Generated EncID: {aliceEncID}")
 	else:
-		print("Failure")
+		print("EncIDs not matching")
 
 EphIDTest()
